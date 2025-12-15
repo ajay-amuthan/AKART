cbcff41783638fa4a603c64da9f503fa8e7389c4
Ajay <kit28.24bcb001@gmail.com>
2025-12-15 13:15:37 +0530

    Starting point of the project

---
+++ src/main/java/com/ecommerce/EcommerceApplication.java
@@ -0,0 +1,11 @@
+package com.ecommerce;
+import org.springframework.boot.SpringApplication; //Spring Boot import
+import org.springframework.boot.autoconfigure.SpringBootApplication; //Spring Boot annotation import
+
+@SpringBootApplication
+public class EcommerceApplication {
+    public static void main(String[] args) {  
+        SpringApplication.run(EcommerceApplication.class, args); 
+        
+    }
+}  
